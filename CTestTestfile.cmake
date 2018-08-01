a983fdbee5a6e16b047b680017b5716e1e365918
Sarah Hashash <hashash2@illinois.edu>
2018-08-01 11:45:03 -0500

    removed files

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hashash2/scope/nccl_scope
-# Build directory: /home/hashash2/scope/nccl_scope
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
